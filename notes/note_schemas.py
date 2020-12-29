1b09d0fa1239cfb444e3aca344ff13e42dced982
devjame <devjames79@gmail.com>
2020-12-29 17:26:21 +0000

    add note schemas

---
+++ notes/note_schemas.py
@@ -0,0 +1,20 @@
+from marshmallow import Schema, fields, post_dump, validate
+
+
+class NoteSchema(Schema):
+    class Meta:
+        ordered = True
+
+    id = fields.Integer(dump_only=True)
+    title = fields.String(required=True, validate=[
+        validate.Length(max=200)])
+    body = fields.String()
+
+    create_at = fields.DateTime(dump_only=True)
+    update_at = fields.DateTime(dump_only=True)
+
+    @post_dump(pass_many=True)
+    def wrap(self, data, many, **kwargs):
+        if many:
+            return {'data': data}
+        return data
