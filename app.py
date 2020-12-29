68f9f77ede2154abbc8dd27563fce1459ed013ff
devjame <devjames79@gmail.com>
2020-12-29 17:24:20 +0000

    register the api resources

--- app.py
+++ app.py
@@ -5,15 +5,12 @@ from flask_migrate import Migrate
 from config import Config
 from extensions import db
 from notes.models import Notes
+from notes.resources.note import NoteResource, NoteListResource
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
-
-    @app.route("/", methods=['GET'])
-    def index():
-        return "<h2>Hello World</h2>"
 
     register_extensions(app)
     register_resources(app)
@@ -27,8 +24,17 @@ def create_app():
 
 def register_extensions(app):
     db.init_app(app)
+    # ma.init_app(app)
     migrate = Migrate(app, db)
 
 
 def register_resources(app):
-    api = Api()
+    api = Api(app)
+
+    api.add_resource(NoteListResource, '/notes')
+    api.add_resource(NoteResource, '/note/<int:note_id>')
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
